d7b6cce69bde42ab160fba22858d91e521563aeb
dasunmaduka <125837525+dasunmaduka@users.noreply.github.com>
2024-10-03 07:35:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Imalka-MD&1vMMZuX5' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '6N8k1RRT#Zujo4A1f1yz06-o9KbH7ad5MIeG1g_CbZdb6ty7fYHM' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94773620112' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
